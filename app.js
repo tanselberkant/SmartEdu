e2470aad7a22f152b54ec06031947a2f866ddff9
tanselberkant <tanselberkant@gmail.com>
2021-09-24 17:18:31 +0300

    Register Page & bcryptPackage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRouter = require('./routes/courseRoute');
 const categoryRouter = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -10,7 +11,7 @@ const app = express();
 mongoose
   .connect('mongodb://localhost/smartEdu-test-db', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('Db Connected Succesfully');
@@ -24,13 +25,14 @@ app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // ROUTES
 app.use('/', pageRoute);
-app.use('/courses',courseRouter);
-app.use('/category',categoryRouter);
+app.use('/courses', courseRouter);
+app.use('/category', categoryRouter);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
