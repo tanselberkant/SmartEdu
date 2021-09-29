e8f41f69c7a4c0c3b53c7b27d2c8e95ea71f123e
tanselberkant <tanselberkant@gmail.com>
2021-09-30 00:44:33 +0300

    Search Bar

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -23,6 +23,7 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
 
     const category = await Category.findOne({ slug: categorySlug });
 
@@ -31,8 +32,23 @@ exports.getAllCourses = async (req, res) => {
     if (categorySlug) {
       filter = { category: category._id };
     }
+    
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    if(query) {
+      filter = {name:query}
+    }
+
+    if(!query && !categorySlug) {
+      filter.name = "",
+      filter.category = null
+    }
+
+    const courses = await Course.find({
+      $or:[
+        {name: { $regex: '.*' + filter.name + '.*', $options: 'i'}},
+        {category: filter.category}
+      ]
+    }).sort('-createdAt').populate('user');
 
     const categories = await Category.find();
 
