cc2f9edaaebeab3dd19ccf196cac652a75b42065
tanselberkant <tanselberkant@gmail.com>
2021-09-29 22:08:30 +0300

    Teacher Dashboard

--- models/Course.js
+++ models/Course.js
@@ -2,38 +2,42 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const Schema = mongoose.Schema;
 
-const CourseSchemaa  = new Schema({
-    name:{
-        type: String,
-        unique: true,
-        required: true
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    createdAt : {
-        type: Date,
-        default: Date.now
-    },
-    slug : {
-        type: String,
-        unique: true
-    },
-    category : {
-        type:mongoose.Schema.Types.ObjectId,
-        ref : 'Category'
-    }
-})
+const CourseSchemaa = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
 
-CourseSchemaa.pre('validate', function(next) {
-    this.slug = slugify(this.name, {
-        lower: true,
-        strict: true
-    })
-    next();
-})
+CourseSchemaa.pre('validate', function (next) {
+  this.slug = slugify(this.name, {
+    lower: true,
+    strict: true,
+  });
+  next();
+});
 
-const Course = mongoose.model('Course',CourseSchemaa);
+const Course = mongoose.model('Course', CourseSchemaa);
 module.exports = Course;
